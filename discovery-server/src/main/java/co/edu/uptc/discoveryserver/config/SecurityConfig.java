1744a1a44a2c3ec28674c8f5a678232f32f77c29
juanSe756 <juansebastiangilborda@gmail.com>
2024-05-18 18:49:04 -0500

    token validation done

--- discovery-server/src/main/java/co/edu/uptc/discoveryserver/config/SecurityConfig.java
+++ discovery-server/src/main/java/co/edu/uptc/discoveryserver/config/SecurityConfig.java
@@ -1,17 +1,17 @@
 package co.edu.uptc.discoveryserver.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.server.SecurityWebFilterChain;
-
-@EnableWebFluxSecurity
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+@Configuration
 public class SecurityConfig {
 
-//    @Bean
-//    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
-//        http
-//                .csrf().disable();
-//        return http.build();
-//    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable);
+        return http.build();
+    }
 }
