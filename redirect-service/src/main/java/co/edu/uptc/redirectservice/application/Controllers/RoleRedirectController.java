5aeb7490d04550340f1a0449252047d734cc725d
juanSe756 <juansebastiangilborda@gmail.com>
2024-05-27 23:41:28 -0500

    Redirect-service redone not tested yet

---
+++ redirect-service/src/main/java/co/edu/uptc/redirectservice/application/Controllers/RoleRedirectController.java
@@ -0,0 +1,34 @@
+package co.edu.uptc.redirectservice.application.Controllers;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.io.IOException;
+
+@RestController
+public class RoleRedirectController {
+    private final RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+    private String getRoleFromAuthentication(Authentication authentication) {
+        return authentication.getAuthorities().stream()
+                .filter(authority -> authority.getAuthority().startsWith("ROLE_"))
+                .map(GrantedAuthority::getAuthority)
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException("No role found in authentication"));
+    }
+    @GetMapping("/redirect")
+    public void redirect(Authentication authentication) throws IOException {
+        String role = getRoleFromAuthentication(authentication);
+        if (role.equals("ROLE_ADMIN")) {
+            redirectStrategy.sendRedirect(null, null, "http://localhost:4200/admin");
+        } else if (role.equals("ROLE_USER")) {
+            redirectStrategy.sendRedirect(null, null, "http://localhost:4200/user");
+        } else {
+            redirectStrategy.sendRedirect(null, null, "http://localhost:4200/error");
+        }
+    }
+
+}
